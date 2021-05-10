8afc4294a707c376e4ef7394dc8f6ab8b83ff7c9
kanade1004 <yoshiki898@ezweb.ne.jp>
2021-05-10 13:49:51 +0900

    コードを良くしました。

--- src/mian.cpp
+++ src/mian.cpp
@@ -1,9 +1,34 @@
-﻿char mojiretsu[1000];
-int mojisuu = 0;
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <stdlib.h>
-#pragma warning(disable : 4996)
-#ifdef this_is_a_great_codo
-問題：1から順番に数字を発言するのだけれども、その数字が3で割り切れれはFizzと発言して、5ならBuzz、１５でならFizzBuzzと発言する
-#endif
-int main() { while (mojisuu++ < 1000)sprintf(mojiretsu, "%d", mojisuu), printf("%s, ", mojisuu % 15 ? mojisuu % 3 ? mojisuu % 5 ? mojiretsu : "Buzz" : "Fizz" : "FizzBuzz"); system("PAUSE"); }
+
+int main() 
+{ 
+	char mojiretsu[1000];
+	int mojisuu = 0;
+
+
+	while (mojisuu++ < 1000)
+	{
+		//第2引数にmojiretsuのメモリサイズを指定し、mojisuuを整数で表示する。
+		sprintf_s(mojiretsu, sizeof(mojiretsu), "%d,", mojisuu);
+
+		//三項演算子では見づらいため、if文を用いる。
+		if (mojisuu % 15 == 0)
+		{
+			printf("FizzBuzz, ");
+		}
+		else if (mojisuu % 5 == 0)
+		{
+			printf("Buzz, ");
+		}
+		else if (mojisuu % 3 == 0)
+		{
+			printf("Fizz, ");
+		}
+		else
+		{
+			printf("%d, ", mojisuu);
+		}
+	}
+	system("PAUSE");
+}
