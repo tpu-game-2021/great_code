5fb70f67209988a0d3e9cebddd77535def42c5bc
kanade1004 <yoshiki898@ezweb.ne.jp>
2021-05-10 14:49:55 +0900

    コードを良くしました。(修正版)

--- src/mian.cpp
+++ src/mian.cpp
@@ -3,15 +3,10 @@
 
 int main() 
 { 
-	char mojiretsu[1000];
 	int mojisuu = 0;
-
 
 	while (mojisuu++ < 1000)
 	{
-		//第2引数にmojiretsuのメモリサイズを指定し、mojisuuを整数で表示する。
-		sprintf_s(mojiretsu, sizeof(mojiretsu), "%d,", mojisuu);
-
 		//三項演算子では見づらいため、if文を用いる。
 		if (mojisuu % 15 == 0)
 		{
