0a3fc6fd0b628ea6fc38009163a92d0955319b95
EvilMaboMan <69211410+DragonKimuchi@users.noreply.github.com>
2021-05-15 02:30:05 +0900

    Update mian.cpp

--- src/mian.cpp
+++ src/mian.cpp
@@ -7,8 +7,8 @@ int mojisuu = 0;
 問題：1から順番に数字を発言するのだけれども、その数字が3で割り切れれはFizzと発言して、5ならBuzz、１５でならFizzBuzzと発言する
 #endif
 int main() { 
-while (mojisuu++ < 1000)sprintf(mojiretsu, "%d", mojisuu), printf("%s, ", mojisuu % 15 ? mojisuu % 3 ? mojisuu % 5 ? mojiretsu : "Buzz" : "Fizz" : "FizzBuzz"); system("PAUSE"); 
-
-
-
+while (mojisuu++ < 1000){
+       sprintf(mojiretsu, "%d", mojisuu),
+        printf("%s, ", mojisuu % 15 ? mojisuu % 3 ? mojisuu % 5 ? mojiretsu : "Buzz" : "Fizz" : "FizzBuzz"); 
+        system("PAUSE"); 
 }
